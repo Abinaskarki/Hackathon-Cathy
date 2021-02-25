63bff130ab770222623261a81c6e3e58985cbcc2
Abinas <youbeapro@gmail.com>
2021-02-25 15:31:48 -0500

    Added DialogFlow messenger code

--- dbhack_front/src/App.js
+++ dbhack_front/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [open, setOpen] = useState(false)
+  
+  const handleOpen = () => {
+    setOpen(!open)
+  }
   return (
     <div>
       <div id="parent">  
         <iframe src="https://www.db.com/careers/index_e.html" width="100%" height="800px" allowFullScreen title="db careers"></iframe>
       </div>
-      <div id= "bot">      
-          <iframe
-              allow="microphone;"
-              width="350"
-              height="430"
-              title="dialog flow"
-              src="https://console.dialogflow.com/api-client/demo/embedded/f7264838-e58e-4512-937e-dc34db785ba8">
-          </iframe>
-        </div>
       </div>
   );
 }
