20dd38c31cd841eafab2491e05036c08771526ad
Abinas <youbeapro@gmail.com>
2021-02-25 10:59:58 -0500

    Added Dialog Flow

--- dbhack_front/src/App.js
+++ dbhack_front/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div>
-        <iframe src="https://www.db.com/careers/index_e.html" height="100%" width="100%"></iframe>
+      <div id="parent">  
+        <iframe src="https://www.db.com/careers/index_e.html" width="100%" height="800px" allowFullScreen title="db careers"></iframe>
       </div>
-    </div>
+      <div class = "bot">      
+          <iframe
+              allow="microphone;"
+              width="350"
+              height="430"
+              title="dialog flow"
+              src="https://console.dialogflow.com/api-client/demo/embedded/f7264838-e58e-4512-937e-dc34db785ba8">
+          </iframe>
+        </div>
+      </div>
   );
 }
 
